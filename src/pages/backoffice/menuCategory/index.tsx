b93f7eeff5c707feaf7c82f6e7cdc7d946dacf6d
MSquare FDC <msquarefdc@gmail.com>
2023-09-13 20:51:27 +0700

    init project

---
+++ src/pages/backoffice/menuCategory/index.tsx
@@ -0,0 +1,11 @@
+import MenuPage from "../menu";
+
+const MenuCategoryPage = () => {
+  return (
+    <div>
+      <h1>MenuCategoryPage</h1>
+    </div>
+  );
+};
+
+export default MenuCategoryPage;
